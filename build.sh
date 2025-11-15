c728f26e5974457c1a7a8092af05ab3447aebcd6
zhu.weijun <zhu.weijun@bestjade.net>
2025-11-15 18:28:48 +0800

    debug

--- build.sh
+++ build.sh
@@ -5,11 +5,13 @@ if [ ! -d "config" ]; then
     exit 1
 fi
 
-for file in "config"/*; do
+mkdir -p bin
+
+for file in "config/*"; do
     if [ -f "$file" ]; then
 		echo "build target [$file]"
         cp -f "$file" "openwrt/.config"
-		make -j4 V=s
+		make -j4
 		IFS="-" read -r part1 part2 part3 <<< "$file"
 		cp -f openwrt/bin/targets/$part1/$part2/*.-squashfs-sysupgrade.bin bin
     fi
