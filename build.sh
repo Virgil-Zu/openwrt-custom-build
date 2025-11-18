a64da74704ef231d2f1c71636a824340c3dea82b
zhu.weijun <zhu.weijun@bestjade.net>
2025-11-18 11:24:50 +0800

    debug

--- build.sh
+++ build.sh
@@ -32,28 +32,25 @@ while IFS= read -r line; do
         wget https://downloads.openwrt.org/releases/${VERSION#v}/targets/${arch}/${soc}/config.buildinfo -O .config
 
         sed -i "/^[[:space:]]*CONFIG_TARGET_DEVICE_/d" .config
-        #sed -i "s/^[[:space:]]*CONFIG_TARGET_MULTI_PROFILE=y/# CONFIG_TARGET_MULTI_PROFILE is not set/g" .config
-        #sed -i "s/^[[:space:]]*CONFIG_DEVEL=y/# CONFIG_DEVEL is not set/g" .config
-        #sed -i "s/^[[:space:]]*CONFIG_TARGET_PER_DEVICE_ROOTFS=y/# CONFIG_TARGET_PER_DEVICE_ROOTFS is not set/g" .config
-        #sed -i "s/^[[:space:]]*CONFIG_AUTOREMOVE=y/# CONFIG_AUTOREMOVE is not set/g" .config
-        #sed -i "s/^[[:space:]]*CONFIG_IB=y/# CONFIG_IB is not set/g" .config
-        #sed -i "s/^[[:space:]]*CONFIG_MAKE_TOOLCHAIN=y/# CONFIG_MAKE_TOOLCHAIN is not set/g" .config
-        #sed -i "s/^[[:space:]]*CONFIG_SDK=y/# CONFIG_SDK is not set/g" .config
-        #sed -i "s/^[[:space:]]*CONFIG_SDK_LLVM_BPF=y/# CONFIG_SDK_LLVM_BPF is not set/g" .config
-        #sed -i "s/^[[:space:]]*CONFIG_TARGET_ALL_PROFILES=y/# CONFIG_TARGET_ALL_PROFILES is not set/g" .config
-        sed -i "s/^[[:space:]]*CONFIG_TARGET_MULTI_PROFILE=y/CONFIG_TARGET_MULTI_PROFILE=n/g" .config
-        sed -i "s/^[[:space:]]*CONFIG_DEVEL=y/CONFIG_DEVEL=n/g" .config
-        sed -i "s/^[[:space:]]*CONFIG_TARGET_PER_DEVICE_ROOTFS=y/CONFIG_TARGET_PER_DEVICE_ROOTFS=n/g" .config
-        sed -i "s/^[[:space:]]*CONFIG_AUTOREMOVE=y/CONFIG_AUTOREMOVE=n/g" .config
-        sed -i "s/^[[:space:]]*CONFIG_IB=y/CONFIG_IB=n/g" .config
-        sed -i "s/^[[:space:]]*CONFIG_MAKE_TOOLCHAIN=y/CONFIG_MAKE_TOOLCHAIN=n/g" .config
-        sed -i "s/^[[:space:]]*CONFIG_SDK=y/CONFIG_SDK=n/g" .config
-        sed -i "s/^[[:space:]]*CONFIG_SDK_LLVM_BPF=y/CONFIG_SDK_LLVM_BPF=n/g" .config
-        sed -i "s/^[[:space:]]*CONFIG_TARGET_ALL_PROFILES=y/CONFIG_TARGET_ALL_PROFILES=n/g" .config
+        sed -i "s/^[[:space:]]*CONFIG_TARGET_MULTI_PROFILE=y/# CONFIG_TARGET_MULTI_PROFILE is not set/g" .config
+        sed -i "s/^[[:space:]]*CONFIG_DEVEL=y/# CONFIG_DEVEL is not set/g" .config
+        sed -i "s/^[[:space:]]*CONFIG_TARGET_PER_DEVICE_ROOTFS=y/# CONFIG_TARGET_PER_DEVICE_ROOTFS is not set/g" .config
+        sed -i "s/^[[:space:]]*CONFIG_AUTOREMOVE=y/# CONFIG_AUTOREMOVE is not set/g" .config
+        sed -i "s/^[[:space:]]*CONFIG_IB=y/# CONFIG_IB is not set/g" .config
+        sed -i "s/^[[:space:]]*CONFIG_MAKE_TOOLCHAIN=y/# CONFIG_MAKE_TOOLCHAIN is not set/g" .config
+        sed -i "s/^[[:space:]]*CONFIG_SDK=y/# CONFIG_SDK is not set/g" .config
+        sed -i "s/^[[:space:]]*CONFIG_SDK_LLVM_BPF=y/# CONFIG_SDK_LLVM_BPF is not set/g" .config
+        sed -i "s/^[[:space:]]*CONFIG_TARGET_ALL_PROFILES=y/# CONFIG_TARGET_ALL_PROFILES is not set/g" .config
+        sed -i "s/^[[:space:]]*CONFIG_COLLECT_KERNEL_DEBUG=y/# CONFIG_COLLECT_KERNEL_DEBUG is not set/g" .config
 
         sed -i "s|downloads.openwrt.org|mirrors.aliyun.com/openwrt|g" .config
         echo "- 设置 opkg 源为 阿里云 \`https://mirrors.aliyun.com/openwrt\`" >> "${RELEASE_FILE}"
 
+        echo "# CONFIG_KERNEL_DEBUG_INFO is not set" >> .config
+        echo "# CONFIG_KERNEL_DEBUG_INFO_REDUCED is not set" >> .config
+        echo "# CONFIG_KERNEL_DEBUG_KERNEL is not set" >> .config
+        echo "# CONFIG_KERNEL_DEBUG_FS is not set" >> .config
+        echo "# CONFIG_TARGET_ROOTFS_INITRAMFS is not set" >> .config
         echo "CONFIG_TARGET_${arch}_${soc}_DEVICE_${device}=y" >> .config
         echo "CONFIG_LUCI_LANG_zh_Hans=y" >> .config
         echo "CONFIG_PACKAGE_luci-i18n-base-zh-cn=y" >> .config
@@ -62,6 +59,7 @@ while IFS= read -r line; do
 
         echo "- 内置简体中文语言包 \`luci-i18n-base-zh-cn\`" >> "${RELEASE_FILE}"
 
+        echo "config..."
         make defconfig
         cat .config
 
@@ -69,7 +67,7 @@ while IFS= read -r line; do
         make download -j$(nproc)
         
         echo "make..."
-        make -j1
+        make -j$(nproc)
 
 		cp -f openwrt/bin/targets/${arch}/${soc}/*-squashfs-sysupgrade.bin bin
 		cd ..
