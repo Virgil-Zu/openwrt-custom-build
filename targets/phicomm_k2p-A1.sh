cfc492ead631d215421e49e8c1617f0b887c4d39
zhu.weijun <zhu.weijun@bestjade.net>
2026-03-11 14:54:24 +0800

    fix dts

--- targets/phicomm_k2p-A1.sh
+++ targets/phicomm_k2p-A1.sh
@@ -3,8 +3,10 @@ set -e
 
 ####################
 #*** patch mt76 max-tx-power
-cp -f patches/701-mt76-mt7615-max-tx-power.patch openwrt/package/kernel/mt76/patches/
+cp -f patches/301-mt76-mt7615-max-tx-power-k2p.patch openwrt/package/kernel/mt76/patches/
 
+target_dts='openwrt/target/linux/ramips/dts/mt7621_phicomm_k2p-A1.dts'
+cp -f openwrt/target/linux/ramips/dts/mt7621_phicomm_k2p.dts "${target_dts}"
 cat <<EOF >> openwrt/target/linux/ramips/image/mt7621.mk
 define Device/phicomm_k2p-A1
   \$(Device/phicomm_k2p)
