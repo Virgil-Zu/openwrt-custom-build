18729b12f06b74fe0584c839dfa7a1e7bc0ab532
zhu.weijun <zhu.weijun@bestjade.net>
2026-03-12 22:02:50 +0800

    fix v24.10.2

--- targets/phicomm_k2p-A1-32m.sh
+++ targets/phicomm_k2p-A1-32m.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 set -e
 
+####################
+#*** patch mt76 max-tx-power
+mkdir -p openwrt/package/kernel/mt76/patches/
+cp -f patches/301-mt76-mt7615-max-tx-power-k2p.patch openwrt/package/kernel/mt76/patches/
+
 target_dts='openwrt/target/linux/ramips/dts/mt7621_phicomm_k2p-A1-32m.dts'
 cp -f openwrt/target/linux/ramips/dts/mt7621_phicomm_k2p.dts "${target_dts}"
 sed -i 's|compatible = "phicomm,k2p"|compatible = "phicomm,k2p-A1-32m", "phicomm,k2p"|' "${target_dts}"
